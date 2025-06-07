70b358ef79c50e614ae8e4565201d56c4df405a9
Shadow's Club 🌹 <shadows.club.vx@gmail.com>
2025-06-06 20:55:01 -0500

    Actualizar info-dueño.js

--- plugins/info-dueño.js
+++ plugins/info-dueño.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
 
   // Crear contacto VCARD
   let list = [{
-    displayName: "Ignacio",
+    displayName: "Sofi",
     vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Sofi\nitem1.TEL;waid=51920487891:51920487891\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/nee\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Perú 🇵🇪;;;;\nitem4.X-ABLabel:País\nEND:VCARD`
   }];
 
